1e35e45c6b530b3750dd49a9d6820117141ea5f2
David Itcovici <daviditco4@gmail.com>
2020-09-12 02:18:09 -0300

    Add basic state and UI

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,53 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answer() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'));
+    var questions = [
+      'Which music genre do you like the most?',
+      'What\'s your favorite color?',
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text(questions[questionIndex]),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answer,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: answer,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: answer,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
